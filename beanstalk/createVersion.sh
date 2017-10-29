8b94a245eae6bff961a3f8a8e5fbfbc2a09ebe02
arun_stefen <arunstefen@gmail.com>
2017-10-29 12:06:29 +0530

    removing SHA

--- beanstalk/createVersion.sh
+++ beanstalk/createVersion.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-SHA1=$1
-
-VERSION=$2
+VERSION=$1
 
 echo "SHA1: $SHA1"
 echo "BRANCH: $VERSION"
 
-EB_BUCKET=express-test
+EB_BUCKET=express-test-ovc
 SOURCE_BUNDLE="${VERSION}.zip"
 S3_KEY="ET/${SOURCE_BUNDLE}"
 
@@ -16,6 +14,7 @@ zip -r "$SOURCE_BUNDLE"  beanstalk/Dockerrun.aws.json beanstalk/.ebextensions/
 sed -i -e "s/:$VERSION/:TAGNAME/" beanstalk/Dockerrun.aws.json
 
 export AWS_DEFAULT_REGION=us-east-1
+# cp "$SOURCE_BUNDLE" "beanstalk/$SOURCE_BUNDLE"
 
 aws s3 cp "$SOURCE_BUNDLE" s3://$EB_BUCKET/$S3_KEY
 aws elasticbeanstalk create-application-version --application-name ExpressTest --version-label $VERSION --source-bundle S3Bucket=$EB_BUCKET,S3Key=$S3_KEY
